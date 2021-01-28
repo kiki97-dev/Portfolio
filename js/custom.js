6f364a42a9f4dbde7bd541cc36814437bf0422c3
cheol1997 <dltmd1502@naver.com>
2021-01-28 23:08:56 +0900

    version1

---
+++ js/custom.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+
+    var $grid;
+
+    $(".loding_bar").animate({ width: "200px" }, 1000)
+
+    setTimeout(function () {
+        $(".loding_bar").addClass("on");
+
+        setTimeout(function () {
+            $("#wrap").css({ margin: "-100px auto 0px" })
+            setTimeout(function () {
+                $("#wrap").css({ width: "150px", height: "150px" })
+                setTimeout(function () {
+                    $("#wrap").css({ width: "300px", height: "300px" });
+                    $("#wrap .box article").eq(0).css({ transform: "rotateY(0deg) translateZ(400px)" })
+                    $("#wrap .box article").eq(1).css({ transform: "rotateY(0deg) translateZ(400px)" })
+                    $("#wrap .box article").eq(2).css({ transform: "rotateY(0deg) translateZ(400px)" })
+                    $("#wrap .box article").eq(3).css({ transform: "rotateY(0deg) translateZ(400px)" })
+                    $("#wrap .box article").eq(4).css({ transform: "rotateY(0deg) translateZ(400px)" })
+                    $("#wrap .box article").eq(5).css({ transform: "rotateY(0deg) translateZ(400px)" })
+                }, 500)
+            }, 1000)
+        }, 500)
+    }, 2000);
+
+
+    setTimeout(function () {
+        $grid = $(".wrap").isotope({//모션을 적용할 박스의 부모 선택자명
+            itemSelector: "section .wrap article",//모션을 적용할 박스 선태자명
+            columnWidth: "section .wrap article",//넓이값을 구할 박스 선택자명
+            transitionDuration: "0.8s",//모션 이동 속도
+            percentPosition: true//넓이값이 퍼센트: true, 고정픽셀값이면 false
+        });
+
+        $("section").addClass("on");
+        $("body").css({ overflow: "auto" });
+        $(".loding").fadeOut(1000);
+
+    }, 5000);
+
+
+    $("#gnb li a").on("click", function (e) {
+        e.preventDefault();
+        var sort = $(this).attr("href");
+
+        $grid.isotope({
+            filter: sort
+        });
+
+        $("#gnb li a").removeClass("on");
+        $(this).addClass("on")
+
+    });
+
+});
