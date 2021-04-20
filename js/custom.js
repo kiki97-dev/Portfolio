d399d7c879d68bbb06e9622b6fe094c655b9793f
cheol1997 <dltmd1502@naver.com>
2021-04-21 02:00:16 +0900

    20210421

--- js/custom.js
+++ js/custom.js
@@ -2,53 +2,12 @@ $(document).ready(function () {
 
     var $grid;
     var wrap = $("#wrap");
+    var body = $("body");
     var article = $("#wrap .box article");
     var lodingbar = $(".loding_bar");
+    var $target = $(".publishing .inner a");
 
-    lodingbar.animate({ width: "200px" }, 1000)
-
-    setTimeout(function () {
-        lodingbar.addClass("on");
-
-        setTimeout(function () {
-            wrap.css({ margin: "-100px auto 0px" });
-            setTimeout(function () {
-                wrap.css({ width: "100px", height: "100px" });
-                article.eq(0).css({ transform: "rotateY(0deg) translateZ(50px)" })
-                article.eq(1).css({ transform: "rotateY(90deg) translateZ(50px)" })
-                article.eq(2).css({ transform: "rotateY(180deg) translateZ(50px)" })
-                article.eq(3).css({ transform: "rotateY(270deg) translateZ(50px)" })
-                article.eq(4).css({ transform: "rotateY(90deg) translateZ(50px)" })
-                article.eq(5).css({ transform: "rotateY(-90deg) translateZ(50px)" })
-                setTimeout(function () {
-                    $("#wrap").css({ width: "300px", height: "300px" });
-                    article.css({ opacity: 0 });
-                    article.eq(0).css({ transform: "rotateY(0deg) translateZ(1000px)" })
-                    article.eq(1).css({ transform: "rotateY(90deg) translateZ(1000px)" })
-                    article.eq(2).css({ transform: "rotateY(180deg) translateZ(1000px)" })
-                    article.eq(3).css({ transform: "rotateY(270deg) translateZ(1000px)" })
-                    article.eq(4).css({ transform: "rotateY(90deg) translateZ(1000px)" })
-                    article.eq(5).css({ transform: "rotateY(-90deg) translateZ(1000px)" })
-                }, 500)
-            }, 1000)
-        }, 500)
-    }, 2000);
-
-
-    setTimeout(function () {
-        $grid = $(".wrap").isotope({//모션을 적용할 박스의 부모 선택자명
-            itemSelector: "section .wrap article",//모션을 적용할 박스 선태자명
-            columnWidth: "section .wrap article",//넓이값을 구할 박스 선택자명
-            transitionDuration: "0.8s",//모션 이동 속도
-            percentPosition: true//넓이값이 퍼센트: true, 고정픽셀값이면 false
-        });
-
-        $("section").addClass("on");
-        $("body").css({ overflow: "auto" });
-        $(".loding").fadeOut(1000);
-
-    }, 4500);
-
+    lodingAni();
 
     $("#gnb li a").on("click", function (e) {
         e.preventDefault();
@@ -62,5 +21,61 @@ $(document).ready(function () {
         $(this).addClass("on")
 
     });
+
+    /*
+    $target.on("click", function (e) {
+        e.preventDefault();
+        body.append(
+            $("<div class='backbg'>")
+        )
+
+    })
+    */
+
+    function lodingAni() {
+        lodingbar.animate({ width: "200px" }, 1000)
+
+        setTimeout(function () {
+            lodingbar.addClass("on");
+
+            setTimeout(function () {
+                wrap.css({ margin: "-100px auto 0px" });
+                setTimeout(function () {
+                    wrap.css({ width: "100px", height: "100px" });
+                    article.eq(0).css({ transform: "rotateY(0deg) translateZ(50px)" })
+                    article.eq(1).css({ transform: "rotateY(90deg) translateZ(50px)" })
+                    article.eq(2).css({ transform: "rotateY(180deg) translateZ(50px)" })
+                    article.eq(3).css({ transform: "rotateY(270deg) translateZ(50px)" })
+                    article.eq(4).css({ transform: "rotateY(90deg) translateZ(50px)" })
+                    article.eq(5).css({ transform: "rotateY(-90deg) translateZ(50px)" })
+                    setTimeout(function () {
+                        $("#wrap").css({ width: "300px", height: "300px" });
+                        article.css({ opacity: 0 });
+                        article.eq(0).css({ transform: "rotateY(0deg) translateZ(1000px)" })
+                        article.eq(1).css({ transform: "rotateY(90deg) translateZ(1000px)" })
+                        article.eq(2).css({ transform: "rotateY(180deg) translateZ(1000px)" })
+                        article.eq(3).css({ transform: "rotateY(270deg) translateZ(1000px)" })
+                        article.eq(4).css({ transform: "rotateY(90deg) translateZ(1000px)" })
+                        article.eq(5).css({ transform: "rotateY(-90deg) translateZ(1000px)" })
+                    }, 500)
+                }, 1000)
+            }, 500)
+        }, 2000);
+
+
+        setTimeout(function () {
+            $grid = $(".wrap").isotope({//모션을 적용할 박스의 부모 선택자명
+                itemSelector: "section .wrap article",//모션을 적용할 박스 선태자명
+                columnWidth: "section .wrap article",//넓이값을 구할 박스 선택자명
+                transitionDuration: "0.8s",//모션 이동 속도
+                percentPosition: true//넓이값이 퍼센트: true, 고정픽셀값이면 false
+            });
+
+            $("section").addClass("on");
+            $("body").css({ overflow: "auto" });
+            $(".loding").fadeOut(1000);
+
+        }, 4500);
+    }
 
 });
